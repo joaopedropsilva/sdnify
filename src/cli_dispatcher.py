0a00ad713eb77ad40787f85f3f7c5cb1e05282c6
joaofauser <joaofausersb@gmail.com>
2024-11-07 10:28:43 -0300

    Adicionando parametros para os comandos

--- src/cli_dispatcher.py
+++ src/cli_dispatcher.py
@@ -10,17 +10,23 @@ def cli_dispatcher():
         help="Escolha a ação a ser executada"
     )
     
+ # Adiciona os parâmetros opcionais conforme necessário
+    parser.add_argument("--file", type=str, help="Caminho de um arquivo JSON para criar a rede")
+    parser.add_argument("--name", type=str, help="Nome da política para criar ou atualizar")
+    parser.add_argument("--protocol", type=str, help="Protocolo da política (ex: HTTP, FTP e RTP)")
+    parser.add_argument("--bandwidth", type=float, help="Largura de banda para a política")
+    
     args = parser.parse_args()
     
-    # Mapeia o argumento para o método correspondente
+   # Mapeia o argumento para o método correspondente, passando parâmetros necessários
     action_map = {
-        "create_network": ActionDirector.create_network, # caminho de um arquivo JSON
-        "destroy_network": ActionDirector.destroy_network, # sem parâmetro
-        "create_policy": ActionDirector.create_policy, # nome, protocolo, bandwidth
-        "update_policy": ActionDirector.update_policy, # nome, protocolo, bandwidth
-        "remove_policy": ActionDirector.remove_policy, # protocolo
-        "show_network_state": ActionDirector.show_network_state, # sem parâmetro
-        "show_manual": ActionDirector.show_manual, # sem parâmetro -> manual inteiro ou rules ou commands
+        "create_network": lambda: ActionDirector.create_network(args.file),  
+        "destroy_network": ActionDirector.destroy_network, 
+        "create_policy": lambda: ActionDirector.create_policy(args.name, args.protocol, args.bandwidth), 
+        "update_policy": lambda: ActionDirector.update_policy(args.name, args.protocol, args.bandwidth),  
+        "remove_policy": lambda: ActionDirector.remove_policy(args.protocol), 
+        "show_network_state": ActionDirector.show_network_state, 
+        "show_manual": ActionDirector.show_manual
     }
     
     # Executa o método correspondente
