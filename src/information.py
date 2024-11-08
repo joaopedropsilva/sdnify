8c37bfd6f9b48a39cd24ee105ef47154c29983da
nathan00pdl <nathanpaiva00@gmail.com>
2024-11-08 18:54:08 -0300

    adding class for json validation

--- src/information.py
+++ src/information.py
@@ -26,6 +26,45 @@ class Utils:
         finally:
             print("Operação de leitura do arquivo concluída.")
 
+class topologyValidator:
+    @staticmethod
+    def validate_json(data: dict) -> bool:
+        try:
+            if not isinstance(data.get("hosts"), list):
+                raise invalidTopologyError("Chave 'Hosts' deve ser do tipo list.")
+            
+            for host in data["hosts"]:
+                if not isinstance(host, str):
+                    raise invalidTopologyError("Cada item da lista 'Hosts' deve ser passado como string.")
+            
+            if not isinstance(data.get("switches"), list):
+                raise invalidTopologyError("Chave 'Switches' deve ser do tipo 'list'.")
+            
+            for switch in data["switches"]:
+                if not isinstance(switch, dict):
+                    raise invalidTopologyError("Chave 'Switches' deve ser composta por uma lista de objetos.")
+                if "id" not in switch or not isinstance(switch["id"], str):
+                    raise invalidTopologyError("Objetos da lista 'Switches' devem conter uma chave 'id' do tipo 'string'.")
+                if "link" not in switch or not isinstance(switch["link"], list):
+                    raise invalidTopologyError("Objetos da lista 'Switches' devem conter uma lista 'link' representando conexões.")
+                
+            for link in switch["link"]:
+                if not isinstance(link, str):
+                    raise invalidTopologyError("Chave 'link' nos objetos da lista 'switches' deve ser do tipo string.")
+
+            return True
+
+        except invalidTopologyError as e:
+            print(e)
+            return False
+        
+        finally:
+            print("Topologia Válida.")
+
+class invalidTopologyError(Exception):
+    def __init__(self, message = "Topologia de rede inválida."):
+        super().__init__(message)
+
 class RoutineResults():
     def __init__(self, status: bool = False, err_reason: str = "", payload = None):
         self.__status = status
