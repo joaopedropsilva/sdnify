81fc5489cdb31aec41a97ec9037e6b0789ecfb9f
nathan00pdl <nathanpaiva00@gmail.com>
2024-11-12 19:59:19 -0300

    validate private method implementation

--- src/qos.py
+++ src/qos.py
@@ -2,13 +2,38 @@ from typing import List
 
 from information import PolicyTypes, RoutineResults
 
+class Policy:
+    def __init__(self, name: str, traffic_type: PolicyTypes, bandwidth_reserved: int):
+        self.name = name
+        self.traffic_type = traffic_type
+        self.bandwidth_reserved = bandwidth_reserved
+
 class FlowManager:
     def __init__(self):
         self.__config: dict = {}
         self.policies: List[Policy] = []
 
     def __validate(self, policy: Policy) -> RoutineResults:
-        pass
+        try:
+            errors = []
+
+            if not policy.name:
+                errors.append("Nome de política inválido.")
+            
+            if not isinstance(policy.traffic_type, PolicyTypes):
+                errors.append("Tipo de tráfego fornecido é inválido.")
+            
+            if not (0 < policy.bandwidth_reserved <= 1000):
+                errors.append("Largura de banda reservada deve estar entre 1 e 100 Mbps.")
+
+            if errors:
+                return RoutineResults(status=False, err_reason="; ".join(errors))
+            
+        except Exception as e:
+            return RoutineResults(status=False, err_reason="Erro de validação: {str(e)}")
+        
+        finally:
+            print("Política validada com sucesso.")
 
     def __init_framework_config(self) -> RoutineResults:
         # inicializa o arquivo de config do controlador
@@ -36,9 +61,4 @@ class FlowManager:
     def remove(self, policy: Policy) -> RoutineResults:
         pass
 
-class Policy:
-    def __init__(self, name: str, traffic_type: PolicyTypes, bandwidth_reserved: int):
-        self.name = name
-        self.traffic_type = traffic_type
-        self.bandwidth_reserved = bandwidth_reserved
 
