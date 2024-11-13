d666e63deee59eabc86e98647de3bad9c323c872
nathan00pdl <nathanpaiva00@gmail.com>
2024-11-13 09:50:38 -0300

    remove method implementation

--- src/qos.py
+++ src/qos.py
@@ -33,12 +33,10 @@ class FlowManager:
     def update(self, policy: Policy) -> RoutineResults:
         try:
             validation = self.__validate(policy)
-
             if validation.status == False:
                 return RoutineResults(status=False, err_reason="Falha na validação da política: " + validation.err_reason)
 
             existing_policy = next((p for p in self.policies if p.name == policy.name), None)
-
             if not existing_policy:
                 return RoutineResults(status=False, err_reason="Política não encontrada.")
             
@@ -54,7 +52,24 @@ class FlowManager:
             print("Operação de atualização de políticas finalizado.")
 
     def remove(self, policy: Policy) -> RoutineResults:
-        pass
+        try:
+            validation = self.__validate(policy)
+            if validation.status == False:
+                return RoutineResults(status=False, err_reason="Falha na validação da política: " + validation.err_reason)
+
+            existing_policy = next((p for p in self.policies if p.name == policy.name), None)
+            if not existing_policy:
+                return RoutineResults(status=False, err_reason="Política não encontrada.")
+            
+            self.policies.remove(existing_policy)
+
+            return RoutineResults(status=True, payload="Política removida com sucesso.")
+        
+        except Exception as e:
+            return RoutineResults(status=False, err_reason="Falha ao remover política: str{e}")
+        
+        finally:
+            print("Operação de remoção de políticas finalizado.")
 
 class Policy:
     def __init__(self, name: str, traffic_type: PolicyTypes, bandwidth_reserved: int):
