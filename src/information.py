65048b827ada62416e0b8835d608ae986c9c1160
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2024-11-14 09:05:17 -0300

    merge with main

--- src/information.py
+++ src/information.py
@@ -1,9 +1,30 @@
 from enum import Enum
 
+from pathlib import Path
+
+import json
+import requests
+
 class Utils:
     @staticmethod
-    def read_file() -> dict:
-        pass
+    def read_file(file_path: str) -> dict:
+        try:
+            file_path = Path(file_path).resolve()
+        
+            if not file_path.exists():
+                raise FileNotFoundError(f"Arquivo não encontrado: {file_path}")
+        
+            with open(file_path, "r", encoding="utf-8") as file:
+                data = json.load(file)
+                
+            return data
+        
+        except FileNotFoundError as e:
+            print(e)
+            return None
+        
+        finally:
+            print("Operação de leitura do arquivo concluída.")
 
 class RoutineResults():
     def __init__(self, status: bool = False, err_reason: str = "", payload = None):
