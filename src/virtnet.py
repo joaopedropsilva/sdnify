69ac2831dab379e9688b9b2de55e9f208d50d4cd
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2024-12-08 09:20:04 -0300

    added topology schema validation to a new class

--- src/virtnet.py
+++ src/virtnet.py
@@ -25,68 +25,49 @@ class CustomTopo(Topo):
                 self.addLink(host, id)
 
 
-class VirtNetBuilder():
-    _DEFAULT_CONTROLLER_NAME = "c0"
-    _DEFAULT_CONTROLLER_IP = "faucet"
-    _DEFAULT_OPF_PORT = 6653
-    _TS_SWITCHES_KEY = "switches"
-    _TS_HOSTS_KEY = "hosts"
-    _TS_ID_KEY = "id"
-    _TS_LINKS_KEY = "links"
+class _TopoSchema:
+    _SWITCHES_KEY = "switches"
+    _HOSTS_KEY = "hosts"
+    _ID_KEY = "id"
+    _LINKS_KEY = "links"
 
-    def __init__(self, topo_schema_path: str, controller_options: dict = {}):
-        self._topo_schema_path = topo_schema_path
-        self._controller_name = controller_options["name"] \
-                                    if len(controller_options) \
-                                    else self._DEFAULT_CONTROLLER_NAME
-        self._controller_ip = controller_options["ip"] \
-                                   if len(controller_options) \
-                                    else self._DEFAULT_CONTROLLER_IP
-        self._opf_port = controller_options["opf_port"] \
-                                    if len(controller_options) \
-                                    else self._DEFAULT_OPF_PORT
+    def __init__(self, topo_schema_path: str):
+        self._schema = File.read_json_from(filepath=topo_schema_path)
 
-    def _create_controller(self) -> RemoteController:
-        return RemoteController(
-            name=self._controller_name,
-            ip=self._controller_ip,
-            port=self._opf_port
-        )
+    @property
+    def schema(self) -> dict:
+        return self._schema
 
-    def _validate_topo(self,
-                        schema: dict) \
-                        -> Success | Error:
-        if not isinstance(schema[self._TS_HOSTS_KEY], list):
+    def validate(self)-> Success | Error:
+        if not isinstance(self._schema[self._HOSTS_KEY], list):
             return Error.HostsKeyWrongTypeInTopoSchema
         
-        for host in schema[self._TS_HOSTS_KEY]:
+        for host in self._schema[self._HOSTS_KEY]:
             if not isinstance(host, str):
                 return Error.HostsKeyWrongValueInTopoSchema
         
-        if not isinstance(schema[self._TS_SWITCHES_KEY], list):
+        if not isinstance(self._schema[self._SWITCHES_KEY], list):
             return Error.SwitchesKeyWrongTypeInTopoSchema
         
-        for switch in schema[self._TS_SWITCHES_KEY]:
+        for switch in self._schema[self._SWITCHES_KEY]:
             if not isinstance(switch, dict):
                 return Error.SwitchesKeyWrongValueInTopoSchema
 
-            if self._TS_ID_KEY not in switch \
-                or not isinstance(switch[self._TS_ID_KEY], str):
+            if self._ID_KEY not in switch \
+                or not isinstance(switch[self._ID_KEY], str):
                 return Error.SwitchObjectWithNoIdInTopoSchema
 
-            if self._TS_LINKS_KEY not in switch \
-                or not isinstance(switch[self._TS_LINKS_KEY], list):
+            if self._LINKS_KEY not in switch \
+                or not isinstance(switch[self._LINKS_KEY], list):
                 return Error.SwitchObjectWithNoLinksInTopoSchema
             
-            for link in switch[self._TS_LINKS_KEY]:
+            for link in switch[self._LINKS_KEY]:
                 if not isinstance(link, str):
                     return Error.LinksWrongValueInTopoSchema
 
         return Success.OperationOk
 
-    def _translate_topo_to_controller_config(self,
-                                             topo_schema: dict) \
-                                             -> Success | Error:
+    def translate_topo_to_controller_config(self) -> Success | Error:
         faucet_path = Path(File.get_project_path(),
                            "dependencies/etc/faucet/faucet.yaml")
 
@@ -97,10 +78,10 @@ class VirtNetBuilder():
             return Error.ControllerConfigNotFound
 
         dps = {}
-        for dp_number, switch in enumerate(topo_schema[self._TS_SWITCHES_KEY],
+        for dp_number, switch in enumerate(self._schema[self._SWITCHES_KEY],
                                            start=1):
-            id = switch[self._TS_ID_KEY]
-            hosts = switch[self._TS_LINKS_KEY]
+            id = switch[self._ID_KEY]
+            hosts = switch[self._LINKS_KEY]
 
             interfaces = {}
             for opf_port, host in enumerate(hosts, start=1):
@@ -143,22 +124,45 @@ class VirtNetBuilder():
         
         return Success.ConfigWriteOk
 
-    def build_network(self) -> tuple[Success | Error, Mininet | None]:
-        topo_schema = File.read_json_from(self._topo_schema_path)
 
-        validation_result = self._validate_topo(schema=topo_schema)
+class VirtNetBuilder():
+    _DEFAULT_CONTROLLER_NAME = "c0"
+    _DEFAULT_CONTROLLER_IP = "faucet"
+    _DEFAULT_OPF_PORT = 6653
+
+    def __init__(self, topo_schema_path: str, controller_options: dict = {}):
+        self._ts = _TopoSchema(topo_schema_path=topo_schema_path)
+        self._controller_name = controller_options["name"] \
+                                    if len(controller_options) \
+                                    else self._DEFAULT_CONTROLLER_NAME
+        self._controller_ip = controller_options["ip"] \
+                                   if len(controller_options) \
+                                    else self._DEFAULT_CONTROLLER_IP
+        self._opf_port = controller_options["opf_port"] \
+                                    if len(controller_options) \
+                                    else self._DEFAULT_OPF_PORT
+
+    def _create_controller(self) -> RemoteController:
+        return RemoteController(
+            name=self._controller_name,
+            ip=self._controller_ip,
+            port=self._opf_port
+        )
+
+    def build_network(self) -> tuple[Success | Error, Mininet | None]:
+        validation_result = self._ts.validate()
         if not isinstance(validation_result, Success):
             return (validation_result, None)
 
         translation_result = \
-            self._translate_topo_to_controller_config(topo_schema=topo_schema)
+            self._ts.translate_topo_to_controller_config()
         if not isinstance(translation_result, Success):
             return (translation_result, None)
 
         net = None
         build_result = None
         try:
-            net = Mininet(topo=CustomTopo(topo_schema=topo_schema),
+            net = Mininet(topo=CustomTopo(topo_schema=self._ts.schema),
                           controller=self._create_controller())
             build_result = Success.NetworkBuildOk
         except Exception:
