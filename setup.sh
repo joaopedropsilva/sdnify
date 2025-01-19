d42d7d927f6128f3ef66ca8c8422fc4a3eeaa240
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2025-01-19 15:43:17 -0300

    removed faucet files versioning

--- setup.sh
+++ setup.sh
@@ -8,14 +8,6 @@ docker volume rm -f sdnify_grafana-storage
 docker volume rm -f sdnify_prometheus-storage
 echo [setup] Deletion complete!
 
-FAUCET_LOG_DIR="$PROJECT_ROOT/etc/faucet"
-echo [setup] Creating faucet log files
-files=("faucet.log" "faucet_exception.log" "gauge.log" "gauge_exception.log")
-for file in "${files[@]}"; do
-    touch "$FAUCET_LOG_DIR/$file"
-done
-echo [setup] Log files creation finished!
-
 echo [setup] Building test environment docker image
 docker build -t sdnify-test-env .
 echo [setup] Test environment successfully built!
